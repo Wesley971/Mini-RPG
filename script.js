16639f0196daf593439895c310281f92c8870ffb
Wesley Abdoul <Abdoulwes@gmail.com>
2025-04-12 14:06:36 +0200

    Modifie les variables de santé du joueur et du gobelin pour utiliser 'let' au lieu de 'const' et améliore la logique d'affichage des dégâts dans la fonction de combat.

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-const playerHp = 20;
-const goblinHp = 10;
+let playerHp = 20;
+let goblinHp = 10;
+
 function fight() { 
-    let degats= Math.floor(Math.random() * 3)+1;
+    let degats = Math.floor(Math.random() * 3) + 1; // entre 1 et 3
     goblinHp = goblinHp - degats;
-    document.getElementById("story").innerHTML = `Tu attaques le gobelin ! Tu lui fais ${degats} dégats !Il lui reste ${goblinHP} HP` ;
+
+    if (goblinHp <= 0) {
+        document.getElementById("story").innerHTML = "Tu as vaincu le gobelin !";
+    } else {
+        document.getElementById("story").innerHTML = `Tu attaques le gobelin ! Tu lui fais ${degats} dégâts ! Il lui reste ${goblinHp} HP.` ;
+    }
 }
