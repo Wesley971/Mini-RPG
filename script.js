0123169bf824ff541c3ac61acd5efc81dc63706b
Wesley Abdoul <Abdoulwes@gmail.com>
2025-04-16 11:47:44 +0200

    Refactor la logique de combat pour utiliser un tableau d'ennemis et améliorer l'affichage des résultats

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 let playerHp = 20;
-let goblinHp = 10;
+let ennemis = [ 
+    {name: "Gobelin", hp: 10},
+    {name : "squelette", hp: 15},
+    {name : "ogre", hp: 20},
+    {name : "dragon", hp: 30},
+];
+let ennemiActuel = ennemis[0]; // Le gobelin
 
 function fight() { 
-    let degatsPlayer = Math.floor(Math.random() * 3) + 1; // entre 1 et 3
-    goblinHp = goblinHp - degatsPlayer;
+    let damagePlayer = Math.floor(Math.random() * 3) + 1; // entre 1 et 3
+    ennemiActuel.hp = ennemiActuel.hp - damagePlayer;
 
-    let degatsGoblin = Math.floor(Math.random() * 4); // entre 0 et 3
-    playerHp = playerHp - degatsGoblin;
+    let damageMonster = Math.floor(Math.random() * 4); // entre 0 et 3
+    playerHp = playerHp - damageMonster;
 
-    if (goblinHp <= 0) {
-        document.getElementById("story").innerHTML = "Tu as vaincu le gobelin !";
+    if (ennemiActuel.hp <= 0) {
+        document.getElementById("story").innerHTML = "Tu as vaincu le monstre !";
     } else if (playerHp <= 0) {
         document.getElementById("story").innerHTML = "Tu es mort !";
     } else {
         document.getElementById("story").innerHTML = 
-            `Tu attaques le gobelin ! Tu lui fais ${degatsPlayer} dégâts ! Il lui reste ${goblinHp} HP. <br>` +
-            `Le gobelin t'attaque ! Il te fait ${degatsGoblin} dégâts ! Il te reste ${playerHp} HP.`;
+            `Tu attaques le ${ennemiActuel.name} ! Tu lui fais ${damagePlayer} dégâts ! Il lui reste ${ennemiActuel.hp} HP. <br>` +
+            `Le ${ennemiActuel.name} t'attaque ! Il te fait ${damageMonster} dégâts ! Il te reste ${playerHp} HP.`;
     }
 }
 function heal() {
